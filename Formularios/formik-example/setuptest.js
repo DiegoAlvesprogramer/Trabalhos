41574c0d88887a51e0e629f99b62f93704489b54
DiegoAlvesprogramer <99756612+DiegoAlvesprogramer@users.noreply.github.com>
2022-05-09 15:44:34 -0300

    Create setuptest.js

---
+++ Formularios/formik-example/setuptest.js
@@ -0,0 +1,4 @@
+// jest-dom adiciona matchers personalizados de jest para declaração em nós DOM.
+// permite que você faça coisas como:
+// expect(element).toHaveTextContent(/react/i)
+import '@testing-library/jest-dom/extend-expect';
